361862e96afaf58997ccf06c3bd4b2d9c38124a9
athena2018 <lesgayard@gmail.com>
2018-04-19 21:44:08 +0200

    material + animation + cdk

--- gali-angular/src/app/app.module.ts
+++ gali-angular/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//warning messages must install depedencies myself
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
