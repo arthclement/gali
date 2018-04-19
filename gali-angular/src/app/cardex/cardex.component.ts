91f0bb750b60918abeb5b13aa6bf39a36533dc45
athena2018 <lesgayard@gmail.com>
2018-04-19 12:34:09 +0200

    route

--- gali-angular/src/app/cardex/cardex.component.ts
+++ gali-angular/src/app/cardex/cardex.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//import { ActivateRoute, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-cardex',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardexComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() {
   }
