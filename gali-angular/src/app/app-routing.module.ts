5d14da85f6fd2e3efa91cb98a7e1a6cefffec76f
athena2018 <lesgayard@gmail.com>
2018-04-19 21:58:05 +0200

    Material Design Setup

--- gali-angular/src/app/app-routing.module.ts
+++ gali-angular/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 // import  add home component
 // import add aboutUs component
 
+//add hammerJs = open source library for gesture support
+import 'hammerjs';
+
 const routes: Routes = [
 
     // place for the paths; object {}
