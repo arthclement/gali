75ef0d09948845c77753836d07b33ecaf66718d9
athena2018 <lesgayard@gmail.com>
2018-04-21 18:40:57 +0200

    cardex-color

--- gali-angular/src/app/app.component.ts
+++ gali-angular/src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 //jquery import
 import * as $ from 'jquery';
 
-
+// properties = components structure
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+//logic here
 export class AppComponent {
   title = 'app';
 }
--- gali-angular/src/app/header/header.component.ts
+++ gali-angular/src/app/header/header.component.ts
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  username: string = "User Example";
+
+  // control the log out button
+  btnSubmit : string = 'Log Out';
+
   constructor() { }
 
   ngOnInit() {
