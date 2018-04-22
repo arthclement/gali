f46771bdf73f4ddaaa15b556f2b630c7d4b08713
athena2018 <lesgayard@gmail.com>
2018-04-22 12:28:21 +0200

    slider component in agenda component ok

--- gali-angular/src/app/agenda/agenda.component.ts
+++ gali-angular/src/app/agenda/agenda.component.ts
@@ -1,4 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+//added module
+import { Component, OnInit, NgModule } from '@angular/core';
+
+//jquery import
+import * as $ from 'jquery';
+
+//slider component import
+import { SliderComponent } from '../slider/slider.component';
+
+//router for calendar path
+import { Routes, RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    SliderComponent,
+    
+
+  ],
+
+  //add Routes
+  imports:[
+    RouterModule.forRoot([
+      {
+        path:'slider', component: SliderComponent
+      },
+      
+
+    ])
+  ]
+})  
+
 
 @Component({
   selector: 'app-agenda',
