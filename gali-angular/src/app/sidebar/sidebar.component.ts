1dbe6da049996ec5b97f963b0efb8047b7604075
athena2018 <lesgayard@gmail.com>
2018-04-22 10:40:18 +0200

    navigation in the sidebar loading too long

--- gali-angular/src/app/sidebar/sidebar.component.ts
+++ gali-angular/src/app/sidebar/sidebar.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
 //jquery import
 import * as $ from 'jquery';
 
+//Router for path
 import { Routes, RouterModule } from '@angular/router';
 import { FormComponent } from '../form/form.component';
 import { BodyComponent } from '../body/body.component';
+import { AgendaComponent } from '../agenda/agenda.component';
 
 //import menu categories
 
-const routes: Routes = [
-  {
-    path: '',
-    component: BodyComponent
-  },
-];
+@NgModule({
+  declarations: [
+    AgendaComponent,
+    FormComponent,
+
+  ],
+  imports:[
+    RouterModule.forRoot([
+      {
+        path:'agenda', component: AgendaComponent
+      },
+      {
+        path:'form', component: FormComponent
+      },
+    ])
+
+  ]
+  
+})
 
 @Component({
   selector: 'app-sidebar',
