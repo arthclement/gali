cbbadd5155058cf24ae789c2cee6230b576fbe7a
GabrielAbrDam <gabriel.abreudamaso@gmail.com>
2018-04-23 13:16:05 +0200

    RDV: creation du component rdv

--- gali-angular/src/app/app.module.ts
+++ gali-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RdvComponent } from './rdv/rdv.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RdvComponent
   ],
   imports: [
     BrowserModule
